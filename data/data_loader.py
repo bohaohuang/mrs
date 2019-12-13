accea27469b83369daf2173e1546e170260b05e8
bohaohuang <bohao.huang@duke.edu>
2019-12-13 10:18:54 -0500

    mixed batch reader

--- data/data_loader.py
+++ data/data_loader.py
@@ -8,10 +8,10 @@ import os
 
 # Libs
 import h5py
+import numpy as np
 from torch.utils import data
 
 # Own modules
-from data import data_utils
 from mrs_utils import misc_utils
 
 
@@ -98,23 +98,40 @@ def get_loader(data_path, file_name, transforms=None):
         raise NotImplementedError('File extension {} is not supportted yet'.format(os.path.splitext(file_name))[-1])
 
 
+class MixedBatchSampler(data.sampler.Sampler):
+    def __init__(self, ds_len, ratio):
+        """
+        Mixed batch sampler where each batch has fixed number of samples from each data source
+        :param ds_len: length of each dataset
+        :param ratio: #samples from each dataset, it should have same number of elements ds_len
+        """
+        super(MixedBatchSampler, self).__init__(ds_len)
+        assert len(ds_len) == len(ratio)
+        self.ds_len = ds_len
+        self.ratio = ratio
+        self.batch_size = sum(ratio)
+        self.offset = [0] + [a for a in self.ds_len[:-1]]
+
+    def __len__(self):
+        return self.ds_len[0]
+
+    def __iter__(self):
+        rand_idx = [np.random.permutation(np.arange(x)) for x in self.ds_len]
+        for cnt in range(self.ds_len[0] // self.batch_size):
+            for ds_cnt, n_sample in enumerate(self.ratio):
+                for curr_cnt in range(n_sample):
+                    yield self.offset[ds_cnt] + rand_idx[ds_cnt][(cnt*n_sample+curr_cnt) % self.ds_len[ds_cnt]]
+
+
 if __name__ == '__main__':
     import albumentations as A
-    from albumentations.pytorch import ToTensorV2
 
-    tsfm = A.Compose([
-        A.Flip(),
-        A.RandomRotate90(),
-        A.Normalize(
-            mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225],
-        ),
-        ToTensorV2(),
-    ])
-    ds = RSDataLoader(r'/hdd/mrs/inria/ps512_pd0_ol0/patches', r'/hdd/mrs/inria/ps512_pd0_ol0/file_list_valid.txt', transforms=tsfm)
-    # ds = RSDataLoader(r'/hdd/pgm/nz_ps512_ol0/patches', r'/hdd/pgm/nz_ps512_ol0/file_list_valid.txt', transforms=tsfm)
-    for cnt, (rgb, gt) in enumerate(ds):
-        data_utils.visualize(rgb, gt)
+    tsfms = [A.RandomCrop(512, 512)]
+    ds1 = RSDataLoader(r'/hdd/mrs/inria/ps512_pd0_ol0/patches', r'/hdd/mrs/inria/ps512_pd0_ol0/file_list_valid.txt', transforms=tsfms)
+    ds2 = RSDataLoader(r'/hdd/mrs/synthinel_1/patches', r'/hdd/mrs/synthinel_1/file_list_train.txt', transforms=tsfms)
+    ds = data.ConcatDataset([ds1, ds2])
+    loader = data.DataLoader(ds, sampler=MixedBatchSampler((len(ds1), len(ds2)), (3, 3)), batch_size=6)
 
-        if cnt == 10:
-            break
+    for cnt, (rgb, gt) in enumerate(loader):
+        from mrs_utils import vis_utils
+        vis_utils.compare_figures(rgb, (2, 3), fig_size=(12, 6))
